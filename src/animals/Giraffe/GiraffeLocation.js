63261c79b366bd3a6eff8abef40e7cc93da8e857
hambonesoftware <126125365+hambonesoftware@users.noreply.github.com>
2025-11-26 16:56:07 -0500

    Add giraffe animal definition and scaffolding

---
+++ src/animals/Giraffe/GiraffeLocation.js
@@ -0,0 +1,8 @@
+// src/animals/Giraffe/GiraffeLocation.js
+import * as THREE from 'three';
+
+export const GiraffeLocation = {
+  position: new THREE.Vector3(0, 0, 0),
+  rotation: new THREE.Euler(0, 0, 0),
+  scale: 1.0
+};
